6c26f68f84f392a9b52d8718cab1d5b05fc617c4
shellteo <shellteo@163.com>
2023-09-18 18:36:44 +0800

    update transactions add l1_*

--- apps/explorer/priv/repo/migrations/20220204060243_transaction_columns_to_support_l2.exs
+++ apps/explorer/priv/repo/migrations/20220204060243_transaction_columns_to_support_l2.exs
@@ -3,10 +3,10 @@ defmodule Explorer.Repo.Migrations.TransactionColumnsToSupportL2 do
 
   def change do
     alter table(:transactions) do
-      add(:l1_fee, :numeric, precision: 100, null: true)
-      add(:l1_fee_scalar, :decimal, null: true)
-      add(:l1_gas_price, :numeric, precision: 100, null: true)
-      add(:l1_gas_used, :numeric, precision: 100, null: true)
+      # add(:l1_fee, :numeric, precision: 100, null: true)
+      # add(:l1_fee_scalar, :decimal, null: true)
+      # add(:l1_gas_price, :numeric, precision: 100, null: true)
+      # add(:l1_gas_used, :numeric, precision: 100, null: true)
       add(:l1_tx_origin, :bytea, null: true)
       add(:l1_block_number, :integer, null: true)
     end
