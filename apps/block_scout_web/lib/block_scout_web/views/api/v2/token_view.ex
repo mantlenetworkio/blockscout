fd00690af9bf18b15ebcb49e91e6a68090841b1c
dennisloh95 <dennisloh95@gmail.com>
2023-11-14 15:40:10 +0800

    feat: add return tag from tokens v2 api

--- apps/block_scout_web/lib/block_scout_web/views/api/v2/token_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/api/v2/token_view.ex
@@ -1,10 +1,46 @@
 defmodule BlockScoutWeb.API.V2.TokenView do
+
+  import BlockScoutWeb.Account.AuthController, only: [current_user: 1]
+  import BlockScoutWeb.Models.GetAddressTags, only: [get_address_tags: 2]
+
   alias BlockScoutWeb.API.V2.Helper
   alias BlockScoutWeb.NFTHelper
   alias Explorer.Chain
   alias Explorer.Chain.Address
 
+  require Logger
+
   @api_true [api?: true]
+
+  def render("token.json", %{token: token, conn: conn}) do
+    %{
+      "address" => Address.checksum(token.contract_address_hash),
+      "symbol" => token.symbol,
+      "name" => token.name,
+      "decimals" => token.decimals,
+      "type" => token.type,
+      "holders" => token.holder_count && to_string(token.holder_count),
+      "exchange_rate" => exchange_rate(token),
+      "total_supply" => token.total_supply,
+      "icon_url" => token.icon_url,
+      "circulating_market_cap" => token.circulating_market_cap,
+      "tags" =>  getTags(conn,token)
+    }
+  end
+
+  defp getTags(conn, token) do
+    %{
+      common_tags: public_tags,
+      personal_tags: private_tags,
+      watchlist_names: watchlist_names
+    } = get_address_tags(token.contract_address_hash, current_user(conn))
+
+    %{
+      "private_tags" => private_tags,
+      "watchlist_names" => watchlist_names,
+      "public_tags" => public_tags
+    }
+  end
 
   def render("token.json", %{token: token}) do
     %{
@@ -36,8 +72,8 @@ defmodule BlockScoutWeb.API.V2.TokenView do
     prepare_token_instance(token_instance, token)
   end
 
-  def render("tokens.json", %{tokens: tokens, next_page_params: next_page_params}) do
-    %{"items" => Enum.map(tokens, &render("token.json", %{token: &1})), "next_page_params" => next_page_params}
+  def render("tokens.json", %{tokens: tokens, next_page_params: next_page_params, conn: conn}) do
+    %{"items" => Enum.map(tokens, &render("token.json", %{token: &1, conn: conn})), "next_page_params" => next_page_params}
   end
 
   def render("token_instances.json", %{
