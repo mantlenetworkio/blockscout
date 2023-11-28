54d1c4ed6c37f2ce5e7df82c2a8f69a290b62f05
dennisloh95 <dennisloh95@gmail.com>
2023-11-28 21:07:27 +0800

    feat: add api v1 format refetch nft metadata api and docs

--- apps/block_scout_web/lib/block_scout_web/controllers/api/rpc/token_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/api/rpc/token_controller.ex
@@ -4,6 +4,10 @@ defmodule BlockScoutWeb.API.RPC.TokenController do
   alias BlockScoutWeb.API.RPC.Helper
   alias Explorer.{Chain, PagingOptions}
 
+  alias Indexer.Fetcher.TokenInstance
+
+  require Logger
+
   def gettoken(conn, params) do
     with {:contractaddress_param, {:ok, contractaddress_param}} <- fetch_contractaddress(params),
          {:format, {:ok, address_hash}} <- to_address_hash(contractaddress_param),
@@ -47,6 +51,45 @@ defmodule BlockScoutWeb.API.RPC.TokenController do
 
       {:format, :error} ->
         render(conn, :error, error: "Invalid contract address hash")
+    end
+  end
+
+  def refetchmetadata(conn, params) do
+    with  {:ok, token_address_hash} <- Map.fetch(params, "addressHash"),
+          {:ok, token_id_str} <- Map.fetch(params, "tokenId") do
+
+      options = [necessity_by_association: %{[contract_address: :smart_contract] => :optional}]
+      with {:ok, hash} <- Chain.string_to_address_hash(token_address_hash),
+      {:ok, token} <- Chain.token_from_address_hash(hash, options),
+      false <- Chain.is_erc_20_token?(token),
+      {token_id, ""} <- Integer.parse(token_id_str),
+      {:ok, token_instance} <-
+        Chain.erc721_or_erc1155_token_instance_from_token_id_and_token_address(token_id, hash) do
+
+        case TokenInstance.Helper.fetch_instance(token_address_hash, token_id) do
+          {:ok, %{metadata: metadata}} ->
+            res = %{
+              "token_id" => token_id_str,
+              "token_contract_address_hash" => token_address_hash,
+              "metadata" => metadata,
+            }
+            render(conn, "refetchmetadata.json", %{result: res})
+          _ ->
+            res = %{
+              "token_id" => token_id_str,
+              "token_contract_address_hash" => token_address_hash,
+            }
+            render(conn, "refetchmetadata.json", %{result: res})
+        end
+
+      else
+      _ ->
+        render(conn, :error, error: "Invalid address hash or token ID")
+      end
+
+    else
+      _ ->
+        render(conn, :error, error: "Invalid address hash or token ID")
     end
   end
 
